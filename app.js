c316d4d482822f2b649c95594605dba87198f9c2
DanielDees <explorers2010@hotmail.com>
2017-03-16 18:16:17 -0400

    Added box object

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ var ctx = canvas.getContext("2d");
 
 var GAME_FPS = 60;
 
+var box = {
+
+	X: 100,
+	Y: 50,
+	H: 20,
+	W: 20,
+}
+
 function drawGame() {
 
 	//X, Y, W, H
-	ctx.fillRect(50, 50, 20, 20);
+	ctx.fillRect(box.X, box.Y, box.W, box.H);
 }
 
 var runGame = setInterval(drawGame(), 1000 / GAME_FPS);
