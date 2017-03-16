ebcb5b137b78b7b62795e2b32500d677cea809c7
DanielDees <explorers2010@hotmail.com>
2017-03-16 18:08:04 -0400

    Game now shows a box

--- app.js
+++ app.js
@@ -3,4 +3,13 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+var GAME_FPS = 60;
+
+function drawGame() {
+
+	//X, Y, W, H
+	ctx.fillRect(50, 50, 20, 20);
+}
+
+var runGame = setInterval(drawGame(), 1000 / GAME_FPS);
 
